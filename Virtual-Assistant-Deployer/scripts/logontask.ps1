0017844e7a9843d928b9a2c236445967596bd5f9
Shashank D <83011430+SD-14@users.noreply.github.com>
2022-10-04 17:38:33 +0530

    Create logontask.ps1

---
+++ Virtual-Assistant-Deployer/scripts/logontask.ps1
@@ -0,0 +1,13 @@
+Start-Transcript -Path C:\WindowsAzure\Logs\logontask.txt -Append
+
+# Install VS COde extension 
+code --install-extension ms-azuretools.vscode-docker
+
+#Install WSL
+wsl --install -d Debian
+
+#Install Dcoker-Desktop
+choco install docker-for-windows -y -force
+
+Unregister-ScheduledTask -TaskName "Setup" -Confirm:$false
+Stop-Transcript
