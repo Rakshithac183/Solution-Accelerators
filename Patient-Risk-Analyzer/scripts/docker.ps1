ae1c181652138a7d3a20a9fbdaad9bbb51679aee
anushabc <66515706+anushabc@users.noreply.github.com>
2022-10-06 18:12:36 +0530

    Update docker.ps1

--- Patient-Risk-Analyzer/scripts/docker.ps1
+++ Patient-Risk-Analyzer/scripts/docker.ps1
@@ -9,7 +9,24 @@ wsl --install -d Debian
 #Install Dcoker-Desktop
 choco install docker-for-windows -y -force
 
-Unregister-ScheduledTask -TaskName "Setup" -Confirm:$false
+#Enable Auto-Logon
+$AutoLogonRegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
+Set-ItemProperty -Path $AutoLogonRegPath -Name "AutoAdminLogon" -Value "1" -type String
+Set-ItemProperty -Path $AutoLogonRegPath -Name "DefaultUsername" -Value "$($env:ComputerName)\demouser" -type String
+Set-ItemProperty -Path $AutoLogonRegPath -Name "DefaultPassword" -Value "Password.1!!" -type String
+Set-ItemProperty -Path $AutoLogonRegPath -Name "AutoLogonCount" -Value "1" -type DWord
 
-Stop-Transcript
+
+# Scheduled Task
+$Trigger= New-ScheduledTaskTrigger -AtLogOn
+$User= "$($env:ComputerName)\demouser"
+$Action= New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe" -Argument "-executionPolicy Unrestricted -File C:\LabFiles\logon.ps1"
+Register-ScheduledTask -TaskName "Setup1" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest -Force
+Set-ExecutionPolicy -ExecutionPolicy bypass -Force
+
+Unregister-ScheduledTask -TaskName "Setup" -Confirm:$false 
+
+Restart-Computer -Force 
+
+
 
